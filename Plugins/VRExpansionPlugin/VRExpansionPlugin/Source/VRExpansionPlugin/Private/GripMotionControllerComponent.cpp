46051c89023d529ea6028a3cd22bb9871bba5138
Joshua <mordentral@gmail.com>
2022-01-06 08:55:03 -0500

    fix expecting c++ interface when the object may have a BP inheiritence
    fix expecting c++ interface when the object may have a BP inheiritence

--- Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -2330,12 +2330,12 @@ void UGripMotionControllerComponent::DropAndSocket_Implementation(const FBPActor
 					}
 				}
 
+				IVRGripInterface::Execute_OnGripRelease(pActor, this, NewDrop, true);
 				if (IVRGripInterface* GripInterface = Cast<IVRGripInterface>(pActor))
 				{
-					GripInterface->Execute_OnGripRelease(pActor, this, NewDrop, true);
 					GripInterface->Native_NotifyThrowGripDelegates(this, false, NewDrop, true);
 				}
-				//IVRGripInterface::Execute_OnGripRelease(pActor, this, NewDrop, true);
+
 			}
 		}
 	}break;
@@ -2397,12 +2397,11 @@ void UGripMotionControllerComponent::DropAndSocket_Implementation(const FBPActor
 					}
 				}
 
+				IVRGripInterface::Execute_OnGripRelease(root, this, NewDrop, true);
 				if (IVRGripInterface* GripInterface = Cast<IVRGripInterface>(root))
 				{
-					GripInterface->Execute_OnGripRelease(root, this, NewDrop, true);
 					GripInterface->Native_NotifyThrowGripDelegates(this, false, NewDrop, true);
 				}
-				//IVRGripInterface::Execute_OnGripRelease(root, this, NewDrop, true);
 			}
 
 			// Call on child grip release on attached parent component
@@ -2521,23 +2520,23 @@ bool UGripMotionControllerComponent::NotifyGrip(FBPActorGripInformation &NewGrip
 				}
 
 				uint8 GripID = NewGrip.GripID;
-				//IVRGripInterface::Execute_OnGrip(pActor, this, NewGrip);
+				IVRGripInterface::Execute_OnGrip(pActor, this, NewGrip);
+				if (!LocallyGrippedObjects.Contains(GripID) && !GrippedObjects.Contains(GripID))
+				{
+					return false;
+				}
+
+				// Now check for c++ specific implementation and throw the native event if we need too
 				if (IVRGripInterface* GripInterface = Cast<IVRGripInterface>(pActor))
 				{
-					GripInterface->Execute_OnGrip(pActor, this, NewGrip);
+					GripInterface->Native_NotifyThrowGripDelegates(this, true, NewGrip, false);
 
 					if (!LocallyGrippedObjects.Contains(GripID) && !GrippedObjects.Contains(GripID))
 					{
 						return false;
 					}
-
-					GripInterface->Native_NotifyThrowGripDelegates(this, true, NewGrip, false);
 				}
 
-				if (!LocallyGrippedObjects.Contains(GripID) && !GrippedObjects.Contains(GripID))
-				{
-					return false;
-				}
 			}
 
 			if (root)
@@ -2605,24 +2604,25 @@ bool UGripMotionControllerComponent::NotifyGrip(FBPActorGripInformation &NewGrip
 				}
 				
 				uint8 GripID = NewGrip.GripID;
-				//IVRGripInterface::Execute_OnGrip(root, this, NewGrip);
+				IVRGripInterface::Execute_OnGrip(root, this, NewGrip);
+				if (!LocallyGrippedObjects.Contains(GripID) && !GrippedObjects.Contains(GripID))
+				{
+					return false;
+				}
 
+				// Now throw the native event if it implements the native interface
 				if (IVRGripInterface* GripInterface = Cast<IVRGripInterface>(root))
 				{
-					GripInterface->Execute_OnGrip(root, this, NewGrip);
+					//GripInterface->Execute_OnGrip(root, this, NewGrip);
+					GripInterface->Native_NotifyThrowGripDelegates(this, true, NewGrip, false);
 
 					if (!LocallyGrippedObjects.Contains(GripID) && !GrippedObjects.Contains(GripID))
 					{
 						return false;
 					}
 
-					GripInterface->Native_NotifyThrowGripDelegates(this, true, NewGrip, false);
 				}
 
-				if (!LocallyGrippedObjects.Contains(GripID) && !GrippedObjects.Contains(GripID))
-				{
-					return false;
-				}
 			}
 
 			if (pActor)
@@ -3192,12 +3192,13 @@ void UGripMotionControllerComponent::Drop_Implementation(const FBPActorGripInfor
 					}
 				}
 
+				IVRGripInterface::Execute_OnGripRelease(pActor, this, NewDrop, false);
 				if (IVRGripInterface* GripInterface = Cast<IVRGripInterface>(pActor))
 				{
-					GripInterface->Execute_OnGripRelease(pActor, this, NewDrop, false);
+					//GripInterface->Execute_OnGripRelease(pActor, this, NewDrop, false);
 					GripInterface->Native_NotifyThrowGripDelegates(this, false, NewDrop, false);
 				}
-				//IVRGripInterface::Execute_OnGripRelease(pActor, this, NewDrop, false);
+
 			}
 		}
 	}break;
@@ -3314,12 +3315,12 @@ void UGripMotionControllerComponent::Drop_Implementation(const FBPActorGripInfor
 					}
 				}
 
+				IVRGripInterface::Execute_OnGripRelease(root, this, NewDrop, false);
 				if (IVRGripInterface* GripInterface = Cast<IVRGripInterface>(root))
 				{
-					GripInterface->Execute_OnGripRelease(root, this, NewDrop, false);
 					GripInterface->Native_NotifyThrowGripDelegates(this, false, NewDrop, false);
 				}
-				//IVRGripInterface::Execute_OnGripRelease(root, this, NewDrop, false);
+
 			}
 
 			// Call on child grip release on attached parent component
